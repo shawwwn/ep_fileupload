c941a16ea2766fd48665200631bc1bc73f1c1180
shawwwn <shawwwn1@gmail.com>
2019-09-09 14:02:56 -0700

    ajax file upload using formData with fallback to hidden iframe

--- static/js/fileUpload.js
+++ static/js/fileUpload.js
@@ -1,10 +1,67 @@
-$(function(){  
-    var info = {  
+$(function(){
+
+    /**
+     * New Ajax Upload File
+     * Use FormData instead of hidden iframe, works on Chrome & FF.
+     */
+    function AjaxUploadNew(btn, conf) {
+            if (btn instanceof jQuery) { btn = btn[0]; } // unwrap
+
+            btn.addEventListener("click", (e) => {
+                e.preventDefault();
+
+                let fileInput = document.createElement("input");
+                fileInput.setAttribute("type", "file");
+                fileInput.onchange = function(e2) {
+                    // chosen a file
+                    let file = fileInput.files[0];
+                    let ext = getExt(file.name);
+                    conf.onChange && conf.onChange(file, ext);
+
+                    // construct form datagram
+                    let formData = new FormData();
+                    formData.append(conf.name, file);
+
+                    // ajax post
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('POST', conf.action, true);
+                    xhr.onreadystatechange = function () {
+                        if(xhr.readyState === 4 && xhr.status === 200) {
+                            let url = xhr.responseText.match(/<body>(.+)<\/body>/i)[1];
+                            console.log(url);
+                            conf.onComplete && conf.onComplete(file, url);
+                        }
+                    };
+
+                    // cancel submit if callback returns false
+                    if (conf.onSubmit) {
+                        let ret = conf.onSubmit(file, ext);
+                        if (typeof ret === "undefined" || !!ret) {
+                            xhr.send(formData);
+                        } else {
+                            xhr.abort();
+                        }
+                    } else {
+                        xhr.send(formData);
+                    }
+                }
+
+                // open file dialog
+                fileInput.click();
+            });
+
+            // get file extension
+            function getExt(file){
+                return (-1 !== file.indexOf('.')) ? file.replace(/.*[.]/, '') : '';
+            }
+        }
+
+    var info = {
       action: '../fileUpload/',
       name: 'uploadfile', 
       onSubmit: function(file, ext){ // On submit we do nothing, it'd be nice to do something but mheh..
       //console.log('Starting...');
-      },  
+      },
       onComplete: function(file, response){
         // Require the editor..
         var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
@@ -20,8 +77,14 @@ $(function(){
         padeditor.ace.replaceRange(undefined, undefined, " " + fileUri + " ");
         // Put the caret back into the pad
         padeditor.ace.focus();
-      }  
+      }
     };
 
-    new AjaxUpload($('#uploadFileSubmit'), info);  
+    if (!! window.FormData) {
+        // ajax upload from formData
+        AjaxUploadNew($('#uploadFileSubmit'), info);
+    } else {
+        // ajax upload to hidden iframe
+        new AjaxUpload($('#uploadFileSubmit'), info);
+    }
 });
